83b75105926b2155f35ad1f30413860be05c3583
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-07 14:42:00 -0500

    Error de vsCode descomento pero el código no se descomenta

--- rutas.py
+++ rutas.py
@@ -1,6 +1,7 @@
 import os
-from conexiones import conexion, guardar
+from utils import seleccionarSgbd
 from validaciones import romperCiclo
+from conexiones import conexion, guardar
 
 def retornarIdSala(idRuta):
     rutas = conexion('rutas')
@@ -11,6 +12,7 @@ def guardarRuta():
     rutas = conexion('rutas')
     salas = conexion('salas')
     temarioRuta = conexion('temarioRuta')
+    print
     bandera = True
     while bandera:
         os.system('clear')
@@ -21,29 +23,37 @@ def guardarRuta():
         else:
             nombre = input("Nombre de la ruta (ejm: Java): ")
             print("***** SGBD *****")
+            db = []
             print("Selecciona 2 sistemas gestores de bases de datos (ejm: 12):\n\t1: Mysql\n\t2: MongoDb\n\t3: Postgresql\n")
             for i in range(0,2):
                 #hay que validar el ingreso de letras en el las DB
                 if i == 0:
                     principal = int(input("Seleccione el SGBD principal de la ruta: "))
+                    db.append(seleccionarSgbd(principal))
                 else:
                     alternativo = int(input("Seleccione el SGBD alternativo de la ruta: "))
-                    
+                    # temarioRuta[idRuta]['bases de datos'].append(seleccionarSgbd(alternativo))
+                    db.append(seleccionarSgbd(alternativo))
+                
             banderaNose = True
+            print("***** BACKEND *****")
+            backend = []
             while True:
-                print("***** BACKEND *****")
                 back = input("Ingrese el framework que que se verá en la ruta: ")
+                backend.append(back)
+                print("tallllll")
                 banderaNose = romperCiclo('otro framework al temario backend')
+            temarioRuta[idRuta] = {'fundamentos': ["Introducción a la algoritmia", " PSeInt", "Python", "Git y gitHub"], "programacion Web": ["HTML", " CSS", "Tailwind"],"programacion formal": [nombre], "bases de datos": db, "backend": backend}
+            guardar('temarioRuta', temarioRuta)
 
-
-            # print(f"\nSeleccione la sala que le va a asignar a la nueva ruta:")
-            # for llave, valor in salas.items():
-            #     print(f"\t{llave}: {valor['nombreS']}")
-            # idSala = input(": ")
-            # if idSala not in salas:
-            #     print(f"\n*** Error - el id {idSala} no existe ***\n")
-            # else:
-            #     print('\n--- Ruta creada con éxito ---\n')
-            #     rutas[idRuta] = {'nombreR': nombre, 'idsalaR': idSala}
-            #     guardar('rutas', rutas)
+            print(f"\nSeleccione la sala que le va a asignar a la nueva ruta:")
+            for llave, valor in salas.items():
+                print(f"\t{llave}: {valor['nombreS']}")
+            idSala = input(": ")
+            if idSala not in salas:
+                print(f"\n*** Error - el id {idSala} no existe ***\n")
+            else:
+                print('\n--- Ruta creada con éxito ---\n')
+                rutas[idRuta] = {'nombreR': nombre, 'idsalaR': idSala}
+                guardar('rutas', rutas)
         bandera = romperCiclo('un nuevo ruta')
