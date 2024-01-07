b938df63ea53070c2c1927b73b1bf8629d65e5e9
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-07 16:08:51 -0500

    Limpié el main

--- main.py
+++ main.py
@@ -10,26 +10,7 @@ campers = con('campers')
 trainers = con('trainers')
 rutas = con('rutas')
 salas = con('salas')
-# pruebas = con('pruebas')
-# rutaTrainer = {}
-# matriculas = {}
 
-
-# campers[1] = {'nombreC': 'Alexis',
-#                 'apellidos': 'Chacón',
-#                 'acudiente': 'Maria Chacón',
-#                 'direccion': 'Floridablanca',
-#                 'telefono': {'celular': [], 'fijo': []},
-#                 'estado': 'inscrito'
-#             }
-# guardar('campers', campers)
-
-# trainer[1] = {'nombreT': 'Miguel'}
-rutas[1] = {'nombreRuta': 'Java'}
-salas[1] = {'sala': 'Sputnik'}
-# pruebas[2] = {'teorico': 0, 'practico': 0, 'promedio': 0}
-# rutaTrainer[1] = {'idRutaT': 1, 'horario': '6-9am'}
-# matriculas[1] = {'idTreinerM': 1, 'idRutaM': 1, 'idSalaM': 1, 'fechaInico': '11-27-2023', 'fechaFin': '06-27-2024'}
 
 # guardarCamper()
 # guardarTrainer()
