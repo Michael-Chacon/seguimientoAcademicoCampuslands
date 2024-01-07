cad71ff6d9add4a9bd9c67ce516843fc9c14aa94
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-07 09:38:33 -0500

    Migré todos los json a una sola carpeta llamada dataBase

--- conexiones.py
+++ conexiones.py
@@ -2,11 +2,11 @@ import json
 
 def conexion(archivo):
     data = None
-    with open(f'{archivo}.json', 'r') as f:
+    with open(f"dataBase/{archivo}.json", "r") as f:
         data =  json.loads(f.read())
     return data
 
 
 def guardar(archivo, diccionario):
-    with open(f"{archivo}.json", "w+") as f:
+    with open(f"dataBase/{archivo}.json", "w+") as f:
         f.write(json.dumps(diccionario, indent=3))
