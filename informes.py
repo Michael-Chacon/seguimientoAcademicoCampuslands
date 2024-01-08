e77288ee385decd6800d4d8a4c53d1a9a44615a5
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-08 05:32:48 -0500

    Listar los campers que se encuentren en estado de inscrito

---
+++ informes.py
@@ -0,0 +1,8 @@
+from utils import mostrarCampersConFiltro as busquedaConFiltro
+
+
+def campersInscritos():
+    print("\n\t*************************")
+    print("\t*  ESTUDIANTES INSCRITOS  *")
+    print("\t*************************\n")
+    busquedaConFiltro('inscrito', 'no')
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from utils import mostrarInfoBasica as listarInfo
 from notas import guardarNotasPrueba as prueba, matricularCamper, filtro
 from camper import guardar as guardarCamper #, mostrarNotasAspirante as relacion
 from trainer import guardar as guardarTrainer, guardarRutaTrainer as rutaTreiner
+from informes import *
 
 campers = con('campers')
 trainers = con('trainers')
@@ -13,20 +14,20 @@ salas = con('salas')
 
 print("Hola, bienvenidos al programa que permite llevar el seguimiento académico de todos los campers")
 
-mainFlag = True
-while mainFlag:
-    os.system('clear')
-    print("\n\t********************")
-    print("\t*  MENÚ PRINCIPAL  *")
-    print("\t********************\n")
+# mainFlag = True
+# while mainFlag:
+#     os.system('clear')
+#     print("\n\t********************")
+#     print("\t*  MENÚ PRINCIPAL  *")
+#     print("\t********************\n")
 
-    print("""
-        1. Todo sobre campers\n
-        2. Todo sobre trainers\n
-        3. Todo sobre rutas\n
-        0. Cerrar sesión\n
-        """)
-    break
+#     print("""
+#         1. Todo sobre campers\n
+#         2. Todo sobre trainers\n
+#         3. Todo sobre rutas\n
+#         0. Cerrar sesión\n
+#         """)
+#     break
 
 # guardarCamper()
 # guardarTrainer()
@@ -40,3 +41,4 @@ while mainFlag:
 # filtro()
 # print(trainers)
 # print(pruebas)
+campersInscritos()
