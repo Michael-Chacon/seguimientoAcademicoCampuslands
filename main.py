9836b20a8e7e0af4d224808d2cca2cbe5a72c5ec
Michael-Chacon <michaelchacon02@gmail.com>
2024-01-07 23:04:41 -0500

    Inicio a crear el menú

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from rutas import guardarRuta
 from conexiones import conexion as con, guardar
 from utils import mostrarInfoBasica as listarInfo
@@ -5,12 +6,27 @@ from notas import guardarNotasPrueba as prueba, matricularCamper, filtro
 from camper import guardar as guardarCamper #, mostrarNotasAspirante as relacion
 from trainer import guardar as guardarTrainer, guardarRutaTrainer as rutaTreiner
 
-
 campers = con('campers')
 trainers = con('trainers')
 rutas = con('rutas')
 salas = con('salas')
 
+print("Hola, bienvenidos al programa que permite llevar el seguimiento académico de todos los campers")
+
+mainFlag = True
+while mainFlag:
+    os.system('clear')
+    print("\n\t********************")
+    print("\t*  MENÚ PRINCIPAL  *")
+    print("\t********************\n")
+
+    print("""
+        1. Todo sobre campers\n
+        2. Todo sobre trainers\n
+        3. Todo sobre rutas\n
+        0. Cerrar sesión\n
+        """)
+    break
 
 # guardarCamper()
 # guardarTrainer()
@@ -21,6 +37,6 @@ salas = con('salas')
 # guardarRuta()
 # prueba(campers)
 # matricularCamper()
-filtro()
+# filtro()
 # print(trainers)
 # print(pruebas)
